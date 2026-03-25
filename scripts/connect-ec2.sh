b77fc287a3e299a665ecd4fc1e6e2555dc5133dc
Sambhav Gupta <sambhav2003gupta@gmail.com>
2026-03-24 17:58:07 -0700

    added bashscriot

---
+++ scripts/connect-ec2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Connect to an EC2 instance via SSH
+
+read -p "Path to PEM file: " PEM_PATH
+read -p "EC2 IP address: " IP_ADDR
+
+# Strip surrounding quotes if user added them
+PEM_PATH=$(echo "$PEM_PATH" | tr -d "'" | tr -d '"')
+IP_ADDR=$(echo "$IP_ADDR" | tr -d "'" | tr -d '"')
+
+# Fix permissions
+chmod 400 "$PEM_PATH"
+
+echo "Connecting to ubuntu@$IP_ADDR..."
+ssh -i "$PEM_PATH" ubuntu@"$IP_ADDR"
